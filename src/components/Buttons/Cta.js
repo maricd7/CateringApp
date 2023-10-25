c3f45dca946e76e069f58390f2444b3fe5152dc1
maricd7 <dejanmarci259@gmail.com>
2023-10-25 07:52:48 +0200

    fixed init tailwind

---
+++ src/components/Buttons/Cta.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Cta() {
+  return (
+    <buton>Order Now</buton>
+  )
+}
+
+export default Cta
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <h4 className='text-white'>CaterEase</h4>
+    </div>
+  )
+}
+
+export default Navbar
