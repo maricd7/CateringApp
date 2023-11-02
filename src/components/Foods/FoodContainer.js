95102cf9f801c8306bc5ba53f3f1979f557c3c3c
maricd7 <87302839+maricd7@users.noreply.github.com>
2023-11-02 09:49:27 +0100

    Add files via upload

--- src/components/Foods/FoodContainer.js
+++ src/components/Foods/FoodContainer.js
@@ -5,33 +5,47 @@ import { CartModal } from "../common";
 
 function FoodContainer() {
   const [modal, setModal] = useState(false);
-  const[existsModal, setExsitsModal]= useState(false)
-  const { foods, addToCart} = useFoodContext();
+  const { foods, addToCart } = useFoodContext();
   const addProductItemToContext = (id) => () => {
     addToCart(id);
     modalSetter();
   };
 
   function modalSetter() {
-      setModal(true);
-      setTimeout(()=>{
-        setModal(false)
-      },3000)}
-    
- 
+    setModal(true);
+    setTimeout(() => {
+      setModal(false);
+    }, 3000);
+  }
+  function calculateDiscountPrice(discount) {
+    return discount > 0 ? discount : 0;
+  }
+  // New Price = Original Price - (Original Price * (Discount Percentage / 100))
   if (!foods) return <div>Loading...</div>;
 
   return (
     <div className="mt-8">
       <ul className="flex flex-wrap gap-16 justify-between relative">
-        {modal && <CartModal text='Added To Cart' btnText='OrderNow'/>}
+        {modal && <CartModal text="Added To Cart" btnText="OrderNow" />}
         {foods.map((meal, index) => (
           <li key={index}>
             <div className="max-w-1/2 rounded-lg relative bg-white p-4 shadow-lg flex flex-col gap-4 border border-zinc-200 hover:scale-110">
-              <img className="rounded-lg w-80 h-72" src={meal.image} alt={meal.name} />
+              {meal.discount > 0 && (
+                <span className="text-white bg-red-600 py-2 px-6 rounded-lg absolute right-0 top-0">
+                  {meal.discount}%
+                </span>
+              )}
+              <img
+                className="rounded-lg w-80 h-72"
+                src={meal.image}
+                alt={meal.name}
+              />
               <h2 className="text-blackTxt">{meal.name}</h2>
               <p className="text-gray-400 h-24">{meal.description}</p>
-              <h2 className="text-blackTxt font-bold">${meal.price}</h2>
+              <div className="flex gap-4 items-center">
+                <h4 className="text-blackTxt font-bold text-3xl">${(meal.price-(meal.price*meal.discount/100)).toFixed(2)}</h4>
+                {meal.discount>0 && (<h4 className="text-gray-500 line-through text-2xl">${meal.price}</h4>)}
+              </div>
               <AddToBag onClick={addProductItemToContext(meal.id)} />
             </div>
           </li>
--- src/data/Data.js
+++ src/data/Data.js
@@ -5,7 +5,8 @@ export const cateringMeals = [
       price: 12.99,
       image: "https://www.aheadofthyme.com/wp-content/uploads/2021/02/cacio-e-pepe-1.jpg",
       description: "Delicious pasta with homemade marinara sauce and garlic bread.",
-      quantity:1
+      quantity:1,
+      discount:25,
     },
     {
       id: 2,
@@ -14,6 +15,7 @@ export const cateringMeals = [
       image: "https://images.unsplash.com/photo-1579366948929-444eb79881eb?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
       description: "Tender steak cooked to perfection, served with mashed potatoes and asparagus.",
       quantity:1,
+      discount:30,
     },
     {
       id: 3,
@@ -22,6 +24,7 @@ export const cateringMeals = [
       image: "https://images.unsplash.com/photo-1544510806-e28d3cd4d4e6?auto=format&fit=crop&q=80&w=2080&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
       description: "A selection of fresh vegetables, tofu, and quinoa in a savory sauce.",
       quantity:1,
+      discount:0,
     },
     {
       id: 4,
@@ -29,7 +32,8 @@ export const cateringMeals = [
       price: 18.99,
       image: "https://images.unsplash.com/photo-1529193591184-b1d58069ecdd?auto=format&fit=crop&q=80&w=1935&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
       description: "Fall-off-the-bone ribs smothered in tangy barbecue sauce, with coleslaw and cornbread.",
-      quantity:1
+      quantity:1,
+      discount:0,
     },
     {
       id: 5,
@@ -37,7 +41,8 @@ export const cateringMeals = [
       price: 15.99,
       image: "https://images.unsplash.com/photo-1570877215023-229052e10c34?auto=format&fit=crop&q=80&w=1935&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
       description: "An assortment of sushi rolls, sashimi, and miso soup for sushi lovers.",
-      quantity:1
+      quantity:1,
+      discount:30,
     },
     {
       id: 6,
@@ -46,6 +51,7 @@ export const cateringMeals = [
       image: "https://images.unsplash.com/photo-1568158879083-c42860933ed7?auto=format&fit=crop&q=80&w=1972&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
       description: "Crisp Greek salad with gyro meat and Tzatziki sauce.",
       quantity:1,
+      discount:0,
     },
     {
       id: 7,
@@ -54,6 +60,7 @@ export const cateringMeals = [
       image: "https://images.unsplash.com/photo-1695606452818-f22013a5c2de?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
       description: "A feast of crab legs, shrimp, and lobster tail, with butter and lemon wedges.",
       quantity:1,
+      discount:10,
     },
     {
       id: 8,
@@ -61,7 +68,8 @@ export const cateringMeals = [
       price: 8.99,
       image: "https://images.unsplash.com/photo-1695649912701-e8431668040f?auto=format&fit=crop&q=80&w=1964&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.jpg",
       description: "Decadent chocolate cake with a rich chocolate ganache and a scoop of vanilla ice cream.",
-      quantity:1
+      quantity:1,
+      discount:50,
     }
   ];
   console.log(cateringMeals);
